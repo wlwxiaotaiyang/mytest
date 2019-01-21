ccf8ba4f3eaf2e1da9eeb3bd95cabdfcd2fdda32
wlwxiaotaiyang <740220602@qq.com>
2019-01-21 17:14:56 +0800

    推送AAA

--- myTest/src/main/java/com/wlw/web/DemoController.java
+++ myTest/src/main/java/com/wlw/web/DemoController.java
@@ -1,7 +1,14 @@
 package com.wlw.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
+
+    @RequestMapping(value = "getUserInfo")
+    public void getUserInfo(){
+
+    }
+
 }
